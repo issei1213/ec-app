66c84a4975b5b5d7bc38a2988f284d449b50961d
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-11-06 14:02:32 +0900

    #9

--- ec-app/src/templates/Home.jsx
+++ ec-app/src/templates/Home.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { getUserId, getUserName } from "../reducks/users/selectors";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const selector = useSelector(state => state);
+  const uid = getUserId(selector);
+  const username = getUserName(selector);
+
   return(
-    <h2>Home</h2>
+    <div>
+      <h2>Home</h2>
+      <p>ユーザーID:{uid}</p>
+      <p>ユーザー名:{username}</p>
+      <p></p>
+    </div>
+
   )
 }
 
