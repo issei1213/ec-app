8295399292cdb877937af3fed7151b0aa5d56d30
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-11-11 00:12:11 +0900

    実施編#6

--- ec-app/src/reducks/products/operation.js
+++ ec-app/src/reducks/products/operation.js
@@ -3,26 +3,29 @@ import { push } from "connected-react-router";
 
 const productsRef = db.collection("products");
 
-export const saveProducts = (name, desicription, category, gender, price, images) => {
+export const saveProducts = (id, name, description, category, gender, price, images, sizes) => {
   return async (dispatch) => {
     const timestamp = FirebaseTimestamp.now()
 
     const data = {
       category: category,
-      desicription: desicription,
+      description: description,
       gender: gender,
       images: images,
       name: name,
       price: parseInt(price, 10),
+      sizes: sizes,
       updated_at: timestamp
     }
 
-    const ref = productsRef.doc();
-    const id = ref.id;
-    data.id = id;
-    data.created_at = timestamp;
+    if (id === "") {
+      const ref = productsRef.doc();
+      id = ref.id;
+      data.id = id;
+      data.created_at = timestamp;
+    }
 
-    return productsRef.doc(id).set(data)
+    return productsRef.doc(id).set(data, {merge: true})
       .then(() => {
         dispatch(push("/"))
       })
--- ec-app/src/templates/ProductEdit.jsx
+++ ec-app/src/templates/ProductEdit.jsx
@@ -1,19 +1,27 @@
-import React, { useState, useCallback } from "react";
-import { TextInput, SelectBox, PrimaryButton } from "../components/UIkit";
+import React, { useState, useCallback, useEffect } from "react";
+import { TextInput, SelectBox, PrimaryButton,  } from "../components/UIkit";
 import { useDispatch } from "react-redux";
 import { saveProducts } from "../reducks/products/operation";
 import ImageArea from "../components/Products/ImageArea";
+import { db } from "../firebase/index" 
+import { SetSizeArea } from "../components/Products";
 
 
 const ProductEdit = () => {
   const dispatch = useDispatch();
+  let id = window.location.pathname.split("/product/edit")[1];
+
+  if (id !== "") {
+    id = id.split("/")[1]
+  }
 
   const [name, setName] = useState(""),
         [description, setDescription] = useState(""),
         [category, setCategory] = useState(""),
         [gender, setGender] = useState(""),
         [images, setImages] = useState([]),
-        [price, setPrice] = useState("");
+        [price, setPrice] = useState(""),
+        [sizes, setSizes] = useState([]);
 
   const inputName = useCallback((event) => {
     setName(event.target.value);
@@ -41,6 +49,22 @@ const ProductEdit = () => {
     {id: "female", name: "女性"},
   ]
 
+  useEffect(() => {
+    if (id !== "") {
+      db.collection("products").doc(id).get()
+        .then(snapshot => {
+          const data = snapshot.data();
+          setName(data.name);
+          setImages(data.images);
+          setGender(data.gender);
+          setPrice(data.price);
+          setDescription(data.description);
+          setCategory(data.category);
+          setSizes(data.sizes);
+        })
+    }
+  }, [id])
+
   return(
     <section>
       <h2 className="u-text__headline u-text-center">商品の登録・編集</h2>
@@ -64,11 +88,13 @@ const ProductEdit = () => {
           fullWidth={true} label={"価格"} multiline={false} required={true}
           onChange={inputPrice} rows={1} value={price} type={"number"}
         />
-        <div className="module-spacer--medium" />
+        <div className="module-spacer--small" />
+        <SetSizeArea sizes={sizes} setSizes={setSizes}/>
+        <div className="module-spacer--small" />
         <div className="center">
           <PrimaryButton
             label={"商品情報を保存"}
-            onClick={() => dispatch(saveProducts(name, description, category, gender, price, images))}
+            onClick={() => dispatch(saveProducts(id, name, description, category, gender, price, images, sizes))}
           />
         </div>
       </div>
